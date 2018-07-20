ff9123104e257d366fee699a7575235da64e0829
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2018-07-20 07:41:15 +0200

    Added controller to list recipes (backend)

---
+++ webApp/Controllers/RecipesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using webApp.Data;
+using webApp.Services.RecipesServices;
+using webApp.Services.RecipesServices.Concrete;
+
+namespace webApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipesController : Controller
+    {
+        private readonly MyDatabaseContext _context;
+
+        public RecipesController(MyDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IQueryable<RecipeListDto> GetAll()
+        {
+            var service = new ListRecipesService(_context);
+            return service.SelectAll();
+        }
+    }
+}
